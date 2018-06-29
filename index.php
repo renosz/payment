4fc7bab186f85b4604d4e783470185db388ec6c4
renosz <32728131+renosz@users.noreply.github.com>
2018-06-29 15:48:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -46,11 +46,17 @@ if($message['type']=='text')
 							'messages' => array(
 array (
   'type' => 'bubble',
-  'styles' => 
+  'hero' => 
   array (
-    'footer' => 
+    'type' => 'image',
+    'url' => 'https://preview.ibb.co/ksutCJ/20180629_054911.jpg',
+    'size' => 'full',
+    'aspectRatio' => '20:13',
+    'aspectMode' => 'cover',
+    'action' => 
     array (
-      'separator' => true,
+      'type' => 'uri',
+      'uri' => 'https://renodoank11.wixsite.com/kcml',
     ),
   ),
   'body' => 
@@ -62,243 +68,160 @@ array (
       0 => 
       array (
         'type' => 'text',
-        'text' => 'ORDER MENU',
+        'text' => 'Contact Team KCML',
         'weight' => 'bold',
-        'color' => '#1DB446',
-        'size' => 'sm',
+        'size' => 'xl',
       ),
       1 => 
       array (
-        'type' => 'text',
-        'text' => 'KCML Team',
-        'weight' => 'bold',
-        'size' => 'xxl',
+        'type' => 'box',
+        'layout' => 'baseline',
         'margin' => 'md',
+        'contents' => 
+        array (
+          0 => 
+          array (
+            'type' => 'icon',
+            'size' => 'sm',
+            'url' => 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png',
+          ),
+          1 => 
+          array (
+            'type' => 'icon',
+            'size' => 'sm',
+            'url' => 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png',
+          ),
+          2 => 
+          array (
+            'type' => 'icon',
+            'size' => 'sm',
+            'url' => 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png',
+          ),
+          3 => 
+          array (
+            'type' => 'icon',
+            'size' => 'sm',
+            'url' => 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png',
+          ),
+          4 => 
+          array (
+            'type' => 'icon',
+            'size' => 'sm',
+            'url' => 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png',
+          ),
+          5 => 
+          array (
+            'type' => 'text',
+            'text' => '4.0',
+            'size' => 'sm',
+            'color' => '#999999',
+            'margin' => 'md',
+            'flex' => 0,
+          ),
+        ),
       ),
       2 => 
       array (
-        'type' => 'text',
-        'text' => 'Selfbot & Bot Protect KCML-TEAMS',
-        'size' => 'xs',
-        'color' => '#aaaaaa',
-        'wrap' => true,
-      ),
-      3 => 
-      array (
-        'type' => 'separator',
-        'margin' => 'xxl',
-      ),
-      4 => 
-      array (
         'type' => 'box',
         'layout' => 'vertical',
-        'margin' => 'xxl',
+        'margin' => 'lg',
         'spacing' => 'sm',
         'contents' => 
         array (
           0 => 
           array (
             'type' => 'box',
-            'layout' => 'horizontal',
+            'layout' => 'baseline',
+            'spacing' => 'sm',
             'contents' => 
             array (
               0 => 
               array (
                 'type' => 'text',
-                'text' => 'Bot Protect',
+                'text' => 'Status',
+                'color' => '#aaaaaa',
                 'size' => 'sm',
-                'color' => '#555555',
-                'flex' => 0,
+                'flex' => 1,
               ),
               1 => 
               array (
                 'type' => 'text',
-                'text' => 'Rp. 50.000',
+                'text' => 'Open',
+                'wrap' => true,
+                'color' => '#666666',
                 'size' => 'sm',
-                'color' => '#111111',
-                'align' => 'end',
+                'flex' => 5,
               ),
             ),
           ),
           1 => 
           array (
             'type' => 'box',
-            'layout' => 'horizontal',
+            'layout' => 'baseline',
+            'spacing' => 'sm',
             'contents' => 
             array (
               0 => 
               array (
                 'type' => 'text',
-                'text' => 'Selfbot Only',
+                'text' => 'Opchat',
+                'color' => '#aaaaaa',
                 'size' => 'sm',
-                'color' => '#555555',
-                'flex' => 0,
+                'flex' => 1,
               ),
               1 => 
               array (
                 'type' => 'text',
-                'text' => 'Rp. 50.000',
+                'text' => '07:00 - 21:00',
+                'wrap' => true,
+                'color' => '#666666',
                 'size' => 'sm',
-                'color' => '#111111',
-                'align' => 'end',
+                'flex' => 5,
               ),
             ),
-          ),
-          2 => 
-          array (
-            'type' => 'box',
-            'layout' => 'horizontal',
-            'contents' => 
-            array (
-              0 => 
-              array (
-                'type' => 'text',
-                'text' => 'SB + /Assist',
-                'size' => 'sm',
-                'color' => '#555555',
-                'flex' => 0,
-              ),
-              1 => 
-              array (
-                'type' => 'text',
-                'text' => 'Rp. 10.000',
-                'size' => 'sm',
-                'color' => '#111111',
-                'align' => 'end',
-              ),
-            ),
-          ),
-          3 => 
-          array (
-            'type' => 'separator',
-            'margin' => 'xxl',
-          ),
-          4 => 
-          array (
-            'type' => 'box',
-            'layout' => 'horizontal',
-            'margin' => 'xxl',
-            'contents' => 
-            array (
-              0 => 
-              array (
-                'type' => 'text',
-                'text' => 'Max order assist',
-                'size' => 'sm',
-                'color' => '#555555',
-              ),
-              1 => 
-              array (
-                'type' => 'text',
-                'text' => '5',
-                'size' => 'sm',
-                'color' => '#111111',
-                'align' => 'end',
-              ),
-            ),
-          ),
-          5 => 
-          array (
-            'type' => 'box',
-            'layout' => 'horizontal',
-            'contents' => 
-            array (
-              0 => 
-              array (
-                'type' => 'text',
-                'text' => 'Waktu sewa',
-                'size' => 'sm',
-                'color' => '#555555',
-              ),
-              1 => 
-              array (
-                'type' => 'text',
-                'text' => '/Bulan',
-                'size' => 'sm',
-                'color' => '#111111',
-                'align' => 'end',
-              ),
-            ),
-          ),
-          6 => 
-          array (
-            'type' => 'box',
-            'layout' => 'horizontal',
-            'contents' => 
-            array (
-              0 => 
-              array (
-                'type' => 'text',
-                'text' => 'Bot protect',
-                'size' => 'sm',
-                'color' => '#555555',
-              ),
-              1 => 
-              array (
-                'type' => 'text',
-                'text' => '5 Bot',
-                'size' => 'sm',
-                'color' => '#111111',
-                'align' => 'end',
-              ),
-            ),
-          ),
-          7 => 
-          array (
-            'type' => 'box',
-            'layout' => 'horizontal',
-            'contents' => 
-            array (
-              0 => 
-              array (
-                'type' => 'text',
-                'text' => 'Max Group Protect',
-                'size' => 'sm',
-                'color' => '#555555',
-              ),
-              1 => 
-              array (
-                'type' => 'text',
-                'text' => '3',
-                'size' => 'sm',
-                'color' => '#111111',
-                'align' => 'end',
-              ),
-            ),
-          ),
-        ),
-      ),
-      5 => 
-      array (
-        'type' => 'separator',
-        'margin' => 'xxl',
-      ),
-      6 => 
-      array (
-        'type' => 'box',
-        'layout' => 'horizontal',
-        'margin' => 'md',
-        'contents' => 
-        array (
-          0 => 
-          array (
-            'type' => 'text',
-            'text' => 'Contact Line ID',
-            'size' => 'xs',
-            'color' => '#aaaaaa',
-            'flex' => 0,
-          ),
-          1 => 
-          array (
-            'type' => 'text',
-            'text' => 'Jaytonmt',
-            'color' => '#aaaaaa',
-            'size' => 'xs',
-            'align' => 'end',
           ),
         ),
       ),
     ),
+  ),
+  'footer' => 
+  array (
+    'type' => 'box',
+    'layout' => 'vertical',
+    'spacing' => 'sm',
+    'contents' => 
+    array (
+      0 => 
+      array (
+        'type' => 'button',
+        'style' => 'link',
+        'height' => 'sm',
+        'action' => 
+        array (
+          'type' => 'uri',
+          'label' => 'Creator',
+          'uri' => 'line.me/ti/p/RHml5-7V9w',
+        ),
+      ),
+      1 => 
+      array (
+        'type' => 'button',
+        'style' => 'link',
+        'height' => 'sm',
+        'action' => 
+        array (
+          'type' => 'uri',
+          'label' => 'Admin',
+          'uri' => 'line.me/ti/p/HvyII50xey',
+        ),
+      ),
+      2 => 
+      array (
+        'type' => 'spacer',
+        'size' => 'sm',
+      ),
+    ),
+    'flex' => 0,
   ),
 )
 							)
